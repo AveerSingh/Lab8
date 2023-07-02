7d7a51ec78ee36ba7bc4a3f2df3813fa6996b2d6
AveerSingh <aveer669@gmail.com>
2023-07-01 19:04:28 -0500

    Lab7

--- src/Lab8.cpp
+++ src/Lab8.cpp
@@ -4,18 +4,17 @@
 
 #include "Particle.h"
 #line 1 "/Users/aveersingh/Lab8/src/Lab8.ino"
-#include <Wire.h>
+#include "Wire.h"
 #include "SparkFun_VCNL4040_Arduino_Library.h"
 #include "oled-wing-adafruit.h"
 #include "blynk.h"
-
 
 void displayProximityValue(unsigned int value);
 void displayLightLevel(unsigned int value);
 void sendToBlynk(unsigned int value);
 void setup();
 void loop();
-#line 7 "/Users/aveersingh/Lab8/src/Lab8.ino"
+#line 6 "/Users/aveersingh/Lab8/src/Lab8.ino"
 OledWingAdafruit display;
 VCNL4040 proximitySensor;
 
@@ -26,10 +25,8 @@ const int LED3 = D5;
 bool useBlynk = false;
 BlynkTimer timer;
 
-unsigned int threshold1 = 60;  
-unsigned int threshold2 = 13000;  
-
-void displayProximityValue(unsigned int value) {
+void displayProximityValue(unsigned int value)
+{
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
@@ -39,7 +36,8 @@ void displayProximityValue(unsigned int value) {
   display.display();
 }
 
-void displayLightLevel(unsigned int value) {
+void displayLightLevel(unsigned int value)
+{
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
@@ -49,69 +47,85 @@ void displayLightLevel(unsigned int value) {
   display.display();
 }
 
-void sendToBlynk(unsigned int value) {
+void sendToBlynk(unsigned int value)
+{
   Blynk.virtualWrite(V1, value);
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   Wire.begin();
-  
-  if (!proximitySensor.begin()) {
+
+  if (!proximitySensor.begin())
+  {
     Serial.println("Device not found. Please check wiring.");
-    while (1);
+    while (1)
+      ;
   }
-  
+
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
   pinMode(LED3, OUTPUT);
-  
+
   display.setup();
   display.clearDisplay();
   display.display();
-  
+
   Blynk.begin("YOUR_AUTH_TOKEN"); // Replace "YOUR_AUTH_TOKEN" with your actual Blynk authentication token
-  timer.setInterval(1000L, []() {
-    Blynk.run();
-  });
+  timer.setInterval(1000L, []()
+                    { Blynk.run(); });
 }
 
-void loop() {
+void loop()
+{
   display.loop();
-  
+
   unsigned int proximityValue = proximitySensor.getProximity();
   unsigned int lightLevel = proximitySensor.getAmbient();
-  
-  if (display.pressedA()) {
+
+  if (display.pressedA())
+  {
     displayProximityValue(proximityValue);
   }
-  
-  if (display.pressedB()) {
+
+  if (display.pressedB())
+  {
     displayLightLevel(lightLevel);
   }
-  
-  if (display.pressedC()) {
+
+  unsigned int threshold1 = 60;
+  unsigned int threshold2 = 13000;
+
+  if (display.pressedC())
+  {
     useBlynk = !useBlynk;
-    delay(500);  // debounce delay to avoid unintended button press
+    delay(500); // debounce delay to avoid unintended button press
     display.clearDisplay();
     display.display();
   }
-  
-  if (proximityValue <= threshold1) {
+
+  if (proximityValue <= threshold1)
+  {
     digitalWrite(LED1, HIGH);
     digitalWrite(LED2, LOW);
     digitalWrite(LED3, LOW);
-  } else if (proximityValue <= threshold2) {
+  }
+  else if (proximityValue <= threshold2)
+  {
     digitalWrite(LED1, LOW);
     digitalWrite(LED2, HIGH);
     digitalWrite(LED3, LOW);
-  } else {
+  }
+  else
+  {
     digitalWrite(LED1, LOW);
     digitalWrite(LED2, LOW);
     digitalWrite(LED3, HIGH);
   }
-  
-  if (useBlynk) {
+
+  if (useBlynk)
+  {
     sendToBlynk(proximityValue);
   }
 }
